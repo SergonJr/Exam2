8a47b0b5a862a26d6e3e01d27aa0d00f135c1688
sergon <sgzzsifuentes@gmail.com>
2020-05-08 09:48:09 -0500

    Second and Third challenges finished

--- Coding-Challenge-2-and-3/models/sport-model.js
+++ Coding-Challenge-2-and-3/models/sport-model.js
@@ -1,8 +1,31 @@
 const mongoose = require( 'mongoose' );
 const uuid = require( 'uuid' );
 
-/* Your code goes here */
+const sportSchema = mongoose.Schema({
+    id: {
+        type: String
+    },
+    name: {
+        type: String
+    },
+    num_players: {
+        type: String
+    }
+});
+
+const sportsCollection = mongoose.model('sports', sportSchema);
+
+const Sports = {
+    deleteSport: function(id)
+    {
+        return sportsCollection.deleteOne({id: id}).then(results => {
+            return results;
+        }).catch(e => {
+            return e;
+        });
+    }
+};
 
 module.exports = {
-    
+    Sports
 };
--- Coding-Challenge-2-and-3/server.js
+++ Coding-Challenge-2-and-3/server.js
@@ -7,8 +7,64 @@ const { DATABASE_URL, PORT } = require( './config' );
 const app = express();
 
 
-/* Your code goes here */
+app.delete('/sports/delete', (req, res) => {
+    let id = req.params.id;
 
+    if (!id)
+    {
+        res.statusMessage = "Id not sent in params";
+        return res.status(406).json();
+    }
+
+    let sportId = req.params.sportId;
+
+    if (!sportId)
+    {
+        res.statusMessage = "Sport Id not sent in params";
+        return res.status(406).json();
+    }
+
+    if (id != sportId)
+    {
+        res.statusMessage = "IDs do not match";
+        return res.status(409).json();
+    }
+
+    let sportToRemove = sports.findIndex( (sport) => {
+        if (sport.id === id && sport.sportId === sportId)
+        {
+            return true;
+        }
+    });
+
+    if (sportToRemove < 0)
+    {
+        res.statusMessage = "Sport not found";
+        return res.status(404).json();
+    }
+
+    sports.splice(sportToRemove, 1);
+    return res.status(204).end();
+})
+
+app.delete('/sportsMongo/delete', (req, res) => {
+    let id = req.params.id;
+
+    Sports.deleteSport(id).then(result => {
+        if (result.deletedCount > 0)
+        {
+            return res.status(200).end();
+        }
+        else
+        {
+            res.statusMessage = "The sport was not found";
+            return res.status(404).json();
+        }
+    }).catch(e => {
+        res.statusMessage = "Something went wrong with the db";
+        return res.status(409).json();
+    })
+})
 
 app.listen( PORT, () => {
     console.log( "This server is running on port 8080" );
